3a560bc5a9a1355e0b4134e4fb43db366abe2d43
mac <mac@macs-MacBook-Air.local>
2024-02-01 22:11:00 -0800

    incomplete-1

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import './App.css';
+import Netflixtopseries from './movieList';
+
+export default function App(){
+  return (
+    <>
+    <Header />
+    <Content/>
+    </>
+  )
+}
+function Header(){
+  return(
+    <div id='heading-bar'>
+      <SearchBar/>
+
+    </div>
+  )
+}
+function SearchBar(){
+  return(
+    <>
+    <input placeholder='Search movie . . .'/>
+    </>
+  )
+}
+function Content(){
+  const [Name,setName]=useState("")
+  const [Image,setImage]=useState("")
+  const [Description,setDescription]=useState("")
+  const [Rating,setRating]=useState(null)
+
+  return(
+    <div id='main-container'>
+      <Listofmovie setName={setName} setDescription={setDescription} setImage={setImage} setRating={setRating}/>
+      <Moviedetail name={Name} image={Image} description={Description} rating={Rating}/>
+    </div>
+  )
+}
+function Listofmovie({setName,setDescription,setImage,setRating}){
+  return(
+    <div id='container-1'>
+      {
+      
+      Netflixtopseries.map((e)=> < ListofmovieItems image = {e.img} series = {e.series} description={e.description} Rating={e.Rating} setName={setName} setImage={setImage} setDescription={setDescription} setRating={setRating}/>)
+      }
+      
+    </div>
+  )
+  }
+function ListofmovieItems({image,series,description,Rating,setName,setDescription,setImage,setRating}){
+
+  function updateCont(){
+    setName(series)
+    setDescription(description)
+    setImage(image)
+    setRating(Rating)
+
+
+  }
+  return (
+    <div className='series-container' onClick={updateCont}>
+      <img src={image} alt={series}/>
+      <span>{series}</span>
+    </div>
+  )
+}
+function Moviedetail({name,image,description,rating}){
+  return(
+    <div id='container-2'>
+      <div id='details' style={name ===""?{display:"none"}:{display:"block"}}>
+        <div id='details-child'>
+          <img src={image} alt={name}id='series-img'/>
+          <span id='series-name'>{name}</span>
+          <div id='imdb-container'>
+            <img src="img/imdb.jpeg" alt='imdb' id='imdb'/>
+            <span>{rating}</span>
+          </div>
+          <div id='details-description'>
+            <span>{description}</span>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
---
+++ src/movieList.js
@@ -0,0 +1,175 @@
+
+  const Netflixtopseries = [
+    { 
+      series: "Stranger Things",
+      description: "In the 1980s, a group of kids in a small town uncover a series of supernatural mysteries when a young boy mysteriously disappears. As they search for answers, they encounter a girl with extraordinary psychokinetic abilities and a government conspiracy involving secret experiments and parallel dimensions.",
+      Rating: 8.7,
+      img: "img/strangerThings.jpeg"
+    },
+    { 
+      series: "The Crown",
+      description: "This historical drama chronicles the reign of Queen Elizabeth II and the major political and historical events that shaped the second half of the 20th century. The series delves into the personal and public life of the Queen, her relationships with world leaders, and the challenges of being a monarch in a rapidly changing world.",
+      Rating: 8.6,
+      img: "img/The Crown.jpeg"
+    },
+    { 
+      series: "The Witcher",
+      description: "Based on the book series by Andrzej Sapkowski, The Witcher follows Geralt of Rivia, a monster hunter known as a Witcher, as he navigates a world filled with dangerous creatures, political intrigue, and powerful sorcery. Along his journey, Geralt encounters a powerful sorceress, a princess with a secret destiny, and a child of surprise who holds the key to a dark prophecy.",
+      Rating: 8.2,
+      img: "img/The Witcher.jpeg"
+    },
+    { 
+      series: "Money Heist",
+      description: "A criminal mastermind known as 'The Professor' recruits a group of eight thieves to carry out an ambitious plan to rob the Royal Mint of Spain. As the heist progresses, tensions rise within the group and with law enforcement, leading to dramatic twists and turns.",
+      Rating: 8.3,
+      img: "img/moneyheist.jpeg"
+    },
+    { 
+      series: "The Umbrella Academy",
+      description: "In this adaptation of the comic book series, a dysfunctional family of adopted sibling superheroes reunites to solve the mystery of their father's death and prevent an impending apocalypse. Each member of the family possesses unique abilities and struggles with their troubled past.",
+      Rating: 8.0,
+      img: "img/The Umbrella Academy.jpeg"
+    },
+    { 
+      series: "Narcos",
+      description: "This crime drama chronicles the rise and fall of notorious drug cartels, focusing on the life of Colombian drug lord Pablo Escobar and the efforts of law enforcement to bring him down. The series explores the complexities of the drug trade and its impact on society.",
+      Rating: 8.8,
+      img: "img/Narcos.jpeg"
+    },
+    { 
+      series: "Black Mirror",
+      description: "An anthology series that explores the dark and often dystopian aspects of modern society and technology. Each episode presents a standalone story that examines the potential consequences of technological advancements and their impact on human behavior.",
+      Rating: 8.8,
+      img: "img/Black Mirror.jpeg"
+    },
+    { 
+      series: "The Queen's Gambit",
+      description: "Set in the 1950s and '60s, this coming-of-age drama follows the life of an orphaned chess prodigy named Beth Harmon as she rises to become a world-renowned chess player while battling addiction and personal demons.",
+      Rating: 8.6,
+      img: "img/The Queen's Gambit.jpeg"
+    },
+    { 
+      series: "Bridgerton",
+      description: "A lavish period drama set in early 19th-century London, Bridgerton follows the lives of the wealthy Bridgerton family as they navigate high society and romantic entanglements. The series offers a diverse cast and a modern twist on classic romance tropes.",
+      Rating: 7.3,
+      img: "img/Bridgerton.jpeg"
+    },
+    { 
+      series: "The Haunting of Hill House",
+      description: "This supernatural horror series follows the Crain family as they confront the traumatic memories of their past and the haunting mysteries of Hill House, the mansion they once lived in. The series explores themes of grief, trauma, and family bonds.",
+      Rating: 8.6,
+      img: "img/The Haunting of Hill House.jpeg"
+    },
+    { 
+      series: "Ozark",
+      description: "A financial planner named Marty Byrde relocates his family to the Ozarks to launder money for a Mexican drug cartel. As Marty gets involved in increasingly dangerous criminal activities, he must navigate the complexities of family life and the criminal underworld.",
+      Rating: 8.4,
+      img: "img/Ozark.jpeg"
+    },
+    { 
+      series: "Dark",
+      description: "This German science fiction thriller follows the interconnected lives of four families as they uncover a time travel conspiracy that spans several generations. As they delve deeper into the mystery, they realize that their past, present, and future are inextricably linked.",
+      Rating: 8.8,
+      img: "img/Dark.jpeg"
+    },
+    { 
+      series: "Breaking Bad",
+      description: "A high school chemistry teacher named Walter White turns to cooking and selling methamphetamine after being diagnosed with terminal cancer. As Walter descends into the criminal underworld, he becomes increasingly ruthless and faces moral dilemmas.",
+      Rating: 9.4,
+      img: "img/Breaking Bad.jpeg"
+    },
+    { 
+      series: "Mindhunter",
+      description: "Set in the late 1970s, this crime thriller follows FBI agents Holden Ford and Bill Tench as they pioneer the development of modern criminal profiling and behavioral science by interviewing imprisoned serial killers. The series explores the psychology of murder and the challenges of understanding criminal behavior.",
+      Rating: 8.6,
+      img: "img/Mindhunter.jpeg"
+    },
+    { 
+      series: "House of Cards",
+      description: "This political drama follows Frank Underwood, a ruthless and cunning politician who manipulates his way to power in Washington, D.C. As Frank rises through the ranks, he schemes, blackmails, and betrays his allies to achieve his ambitions.",
+      Rating: 8.7,
+      img: "img/House of Cards.jpeg"
+    },
+    { 
+      series: "Orange Is the New Black",
+      description: "Based on the memoir of Piper Kerman, this comedic drama follows Piper Chapman, a privileged woman who is sentenced to a year in a women's federal prison for her involvement in a drug smuggling operation. Piper navigates the challenges of prison life and forms friendships with fellow inmates.",
+      Rating: 8.1,
+      img: "img/Orange Is the New Black.jpeg"
+    },
+    { 
+      series: "BoJack Horseman",
+      description: "This animated comedy-drama follows BoJack Horseman, a washed-up sitcom star who struggles with addiction, self-destructive behavior, and existential crises. As BoJack navigates the absurdities of Hollywood and relationships, he grapples with his past and attempts to find meaning in his life.",
+      Rating: 8.7,
+      img: "img/BoJack Horseman.jpeg"
+    },
+    { 
+      series: "Marvel's Daredevil",
+      description: "Blinded as a young boy, Matt Murdock gains extraordinary senses that enable him to fight crime as the masked vigilante Daredevil. As Daredevil, Matt battles the criminal underworld of Hell's Kitchen while grappling with moral dilemmas and personal demons.",
+      Rating: 8.6,
+      img: "img/Marvel's Daredevil.jpeg"
+    },
+    { 
+      series: "Marvel's Jessica Jones",
+      description: "Following a traumatic experience that left her with superhuman abilities, Jessica Jones becomes a private investigator in New York City. Jessica's past comes back to haunt her when she encounters the manipulative and mind-controlling villain Kilgrave.",
+      Rating: 7.9,
+      img: "img/Marvel's Jessica Jones.jpeg"
+    },
+    { 
+      series: "Marvel's Luke Cage",
+      description: "After gaining superhuman strength and unbreakable skin from a sabotaged experiment, Luke Cage becomes a reluctant hero and protector of Harlem. Luke battles crime and corruption while confronting his past and navigating his role as a symbol of hope for the community.",
+      Rating: 7.3,
+      img: "img/Marvel's Luke Cage.jpeg"
+    },
+    { 
+      series: "The Punisher",
+      description: "After the murder of his family, Marine veteran Frank Castle becomes the vigilante known as The Punisher. Armed with his military training and thirst for vengeance, Frank wages a one-man war on crime while uncovering a conspiracy that goes deeper than he ever imagined.",
+      Rating: 8.5,
+      img: "img/The Punisher.jpeg"
+    },
+    { 
+      series: "Marvel's The Defenders",
+      description: "This crossover miniseries brings together the street-level superheroes Daredevil, Jessica Jones, Luke Cage, and Iron Fist to defend New York City from a common threat. As they join forces, the heroes must overcome their differences and work together to save the city.",
+      Rating: 7.3,
+      img: "img/Marvel's The Defenders.jpeg"
+    },
+    { 
+      series: "The Society",
+      description: "When a group of teenagers returns from a school trip to find their town deserted, they must create their own society to survive. As they grapple with power struggles, love triangles, and moral dilemmas, they uncover dark secrets about themselves and their families.",
+      Rating: 6.5,
+      img: "img/The Society.jpeg"
+    },
+    { 
+      series: "Cobra Kai",
+      description: "Set decades after the events of The Karate Kid, this series follows Johnny Lawrence and Daniel LaRusso as they reopen the Cobra Kai and Miyagi-Do dojos, reigniting their rivalry. As old conflicts resurface, Johnny and Daniel confront their past and mentor a new generation of karate students.",
+      Rating: 8.6,
+      img: "img/Cobra Kai.jpeg"
+    },
+    { 
+      series: "Altered Carbon",
+      description: "In a future where consciousness can be transferred to different bodies, a former soldier named Takeshi Kovacs is revived in a new body to solve a wealthy man's murder. As Kovacs delves into the seedy underworld of a futuristic society, he uncovers dark secrets about himself and the powerful elite.",
+      Rating: 8.0,
+      img: "img/Altered Carbon.jpeg"
+    },
+    { 
+      series: "The Dragon Prince",
+      description: "In the magical land of Xadia, two human princes team up with an elven assassin to bring peace to their warring nations. Along their journey, they encounter mythical creatures, magical relics, and dark forces that threaten to plunge the world into chaos.",
+      Rating: 8.4,
+      img: "img/The Dragon Prince.jpeg"
+    },
+    { 
+      series: "Dark Desire",
+      description: "A passionate affair between a married woman and a younger man turns deadly when they become entangled in a web of lies, deceit, and murder. As their dark desires consume them, they must confront the consequences of their actions before it's too late.",
+      Rating: 6.5,
+      img: "img/Dark Desire.jpeg"
+    },
+    { 
+      series: "Emily in Paris",
+      description: "When ambitious marketing executive Emily Cooper lands her dream job in Paris, she embarks on a journey of self-discovery while navigating the challenges of adapting to French culture and finding love in the City of Light.",
+      Rating: 7.0,
+      img: "img/Emily in Paris.jpeg"
+    }
+  ];
+  
+  
+  export default Netflixtopseries;
+  
